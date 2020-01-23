b2980995e3cc98aec115f94995957012806770ca
unknown <pmnwijewardena@gmail.com>
2020-01-24 01:36:52 +0530

    Camera Source Update

--- app/src/main/java/com/example/helperbot/CameraSource.java
+++ app/src/main/java/com/example/helperbot/CameraSource.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.ImageFormat;
 import android.graphics.Rect;
 import android.graphics.RectF;
@@ -27,6 +28,9 @@ import java.util.ArrayList;
 import java.util.IdentityHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * Manages the camera and allows UI updates on top of it (e.g. overlaying extra Graphics or
@@ -37,6 +41,7 @@ import java.util.Map;
 public class CameraSource {
     @SuppressLint("InlinedApi")
     public static final int CAMERA_FACING_BACK = CameraInfo.CAMERA_FACING_BACK;
+    public Classifier imageDetector;
 
     @SuppressLint("InlinedApi")
     public static final int CAMERA_FACING_FRONT = CameraInfo.CAMERA_FACING_FRONT;
@@ -76,6 +81,13 @@ public class CameraSource {
     private  int requestedPreviewWidth = 640;
     private  int requestedPreviewHeight = 480;
     private final boolean requestedAutoFocus = false;
+    private static final String TF_OD_API_MODEL_FILE = "detecty.tflite";
+    private static final String TF_OD_API_LABELS_FILE = "labelmap.txt";
+
+    //    private static final int TF_OD_API_INPUT_SIZE_HEIGHT = ;
+    private static final int TF_OD_API_INPUT_SIZE = 300;
+    private static final boolean TF_OD_API_IS_QUANTIZED = false;
+
 
     // These instances need to be held onto to avoid GC of their underlying resources.  Even though
     // these aren't used outside of the method that creates them, they still must have hard
@@ -94,9 +106,10 @@ public class CameraSource {
      * Dedicated thread and associated runnable for calling into the detector with frames, as the
      * frames become available from the camera.
      */
-    private Thread processingThread;
-
+    private ExecutorService threadPoolExecutor;
+    private Thread barcodeDetectionThread;
     private final FrameProcessingRunnable processingRunnable;
+    private final BarcodeProcessingRunnabale barcodeProcessingRunnabale;
 
     private final Object processorLock = new Object();
     // @GuardedBy("processorLock")
@@ -118,6 +131,7 @@ public class CameraSource {
         graphicOverlay = overlay;
         graphicOverlay.clear();
         processingRunnable = new FrameProcessingRunnable();
+        barcodeProcessingRunnabale =new BarcodeProcessingRunnabale();
 
         if (Camera.getNumberOfCameras() == 1) {
             CameraInfo cameraInfo = new CameraInfo();
@@ -163,9 +177,11 @@ public class CameraSource {
 
         camera.startPreview();
 
-        processingThread = new Thread(processingRunnable);
+        threadPoolExecutor = Executors.newFixedThreadPool(2);
         processingRunnable.setActive(true);
-        processingThread.start();
+        barcodeProcessingRunnabale.setActive(true);
+        threadPoolExecutor.execute(processingRunnable);
+        threadPoolExecutor.execute(barcodeProcessingRunnabale);
         return this;
     }
 
@@ -186,9 +202,11 @@ public class CameraSource {
         camera.setPreviewDisplay(surfaceHolder);
         camera.startPreview();
 
-        processingThread = new Thread(processingRunnable);
+        threadPoolExecutor = Executors.newFixedThreadPool(2);
         processingRunnable.setActive(true);
-        processingThread.start();
+        barcodeProcessingRunnabale.setActive(true);
+        threadPoolExecutor.execute(processingRunnable);
+        threadPoolExecutor.execute(barcodeProcessingRunnabale);
 
         usingSurfaceTexture = false;
         return this;
@@ -205,16 +223,16 @@ public class CameraSource {
      */
     public synchronized void stop() {
         processingRunnable.setActive(false);
-        if (processingThread != null) {
-            try {
-                // Wait for the thread to complete to ensure that we can't have multiple threads
-                // executing at the same time (i.e., which would happen if we called start too
-                // quickly after stop).
-                processingThread.join();
-            } catch (InterruptedException e) {
-                Log.d(TAG, "Frame processing thread interrupted on release.");
-            }
-            processingThread = null;
+        if (threadPoolExecutor != null) {
+//            try {
+//                // Wait for the thread to complete to ensure that we can't have multiple threads
+//                // executing at the same time (i.e., which would happen if we called start too
+//                // quickly after stop).
+//                //threadPoolExecutor.join();
+//            } catch (InterruptedException e) {
+//                Log.d(TAG, "Frame processing thread interrupted on release.");
+//            }
+//            processingThread = null;
         }
 
         if (camera != null) {
@@ -609,6 +627,7 @@ public class CameraSource {
         @Override
         public void onPreviewFrame(byte[] data, Camera camera) {
             processingRunnable.setNextFrame(data, camera);
+            barcodeProcessingRunnabale.setNextFrame(data,camera);
         }
     }
 
@@ -632,6 +651,145 @@ public class CameraSource {
      * associated processing is done for the previous frame, detection on the mostly recently received
      * frame will immediately start on the same thread.
      */
+    private class BarcodeProcessingRunnabale implements Runnable{
+        private final Object lock = new Object();
+        private boolean active = true;
+
+        // These pending variables hold the state associated with the new frame awaiting processing.
+        private ByteBuffer pendingFrameData;
+
+        BarcodeProcessingRunnabale() {
+            try{
+                imageDetector = TFLiteObjectDetectionAPIModel.create(
+                        activity.getAssets(),
+                        TF_OD_API_MODEL_FILE,
+                        TF_OD_API_LABELS_FILE,
+                        TF_OD_API_INPUT_SIZE,
+                        TF_OD_API_IS_QUANTIZED);
+            }
+            catch(IOException e){
+
+            }
+
+        }
+
+        /**
+         * Releases the underlying receiver. This is only safe to do after the associated thread has
+         * completed, which is managed in camera source's release method above.
+         */
+        @SuppressLint("Assert")
+        void release() {
+            assert (threadPoolExecutor.isTerminated());
+        }
+
+        /** Marks the runnable as active/not active. Signals any blocked threads to continue. */
+        void setActive(boolean active) {
+            synchronized (lock) {
+                this.active = active;
+                lock.notifyAll();
+            }
+        }
+
+        /**
+         * Sets the frame data received from the camera. This adds the previous unused frame buffer (if
+         * present) back to the camera, and keeps a pending reference to the frame data for future use.
+         */
+        void setNextFrame(byte[] data, Camera camera) {
+            synchronized (lock) {
+                if (pendingFrameData != null) {
+                    camera.addCallbackBuffer(pendingFrameData.array());
+                    pendingFrameData = null;
+                }
+
+                if (!bytesToByteBuffer.containsKey(data)) {
+                    Log.d(
+                            TAG,
+                            "Skipping frame. Could not find ByteBuffer associated with the image "
+                                    + "data from the camera.");
+                    return;
+                }
+
+                pendingFrameData = bytesToByteBuffer.get(data);
+
+                // Notify the processor thread if it is waiting on the next frame (see below).
+                lock.notifyAll();
+            }
+        }
+
+        /**
+         * As long as the processing thread is active, this executes detection on frames continuously.
+         * The next pending frame is either immediately available or hasn't been received yet. Once it
+         * is available, we transfer the frame info to local variables and run detection on that frame.
+         * It immediately loops back for the next frame without pausing.
+         *
+         * <p>If detection takes longer than the time in between new frames from the camera, this will
+         * mean that this loop will run without ever waiting on a frame, avoiding any context switching
+         * or frame acquisition time latency.
+         *
+         * <p>If you find that this is using more CPU than you'd like, you should probably decrease the
+         * FPS setting above to allow for some idle time in between frames.
+         */
+        @SuppressLint("InlinedApi")
+        @SuppressWarnings("GuardedBy")
+        @Override
+        public void run() {
+            ByteBuffer data;
+
+            while (true) {
+                synchronized (lock) {
+                    while (active && (pendingFrameData == null)) {
+                        try {
+                            // Wait for the next frame to be received from the camera, since we
+                            // don't have it yet.
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            Log.d(TAG, "Frame processing loop terminated.", e);
+                            return;
+                        }
+                    }
+
+                    if (!active) {
+                        // Exit the loop once this camera source is stopped or released.  We check
+                        // this here, immediately after the wait() above, to handle the case where
+                        // setActive(false) had been called, triggering the termination of this
+                        // loop.
+                        return;
+                    }
+
+                    // Hold onto the frame data locally, so that we can use this for detection
+                    // below.  We need to clear pendingFrameData to ensure that this buffer isn't
+                    // recycled back to the camera before we are done using that data.
+                    data = pendingFrameData;
+                    pendingFrameData = null;
+                }
+
+                // The code below needs to run outside of synchronization, because this will allow
+                // the camera to add pending frame(s) while we are running detection on the current
+                // frame.
+
+                try {
+                    synchronized (processorLock) {
+                        Log.d(TAG, "Process an image");
+                        Bitmap bitmap = BitmapUtils.getBitmap(data,  new FrameMetadata.Builder()
+                                .setWidth(previewSize.getWidth())
+                                .setHeight(previewSize.getHeight())
+                                .setRotation(rotation)
+                                .setCameraFacing(facing)
+                                .build());
+                        Bitmap bitmap_scaled = Bitmap.createScaledBitmap(bitmap, 300, 300,false);
+                        final List<Classifier.Recognition> results = imageDetector.recognizeImage(bitmap);
+
+                    }
+                } catch (Throwable t) {
+                    Log.e(TAG, "Exception thrown from receiver.", t);
+                } finally {
+
+                    camera.addCallbackBuffer(data.array());
+
+                }
+            }
+        }
+    }
     private class FrameProcessingRunnable implements Runnable {
 
         // This lock guards all of the member variables below.
@@ -649,7 +807,7 @@ public class CameraSource {
          */
         @SuppressLint("Assert")
         void release() {
-            assert (processingThread.getState() == State.TERMINATED);
+            assert (threadPoolExecutor.isTerminated());
         }
 
         /** Marks the runnable as active/not active. Signals any blocked threads to continue. */
